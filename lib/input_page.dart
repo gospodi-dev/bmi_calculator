eabc007531d3ecaecf1004a53af8eab24f47f064
gospodi <gospodijazzet@gmail.com>
2020-05-21 22:15:39 +0300

    Добавлен значок MALE Добавлен пакет font_awesome_flutter https://pub.dev/packages/font_awesome_flutter

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xff323244);
@@ -21,7 +23,29 @@ class _InputPageState extends State<InputPage> {
             Expanded(
                 child: Row(
               children: <Widget>[
-                Expanded(child: ReusableCard(colour: activeCardColor)),
+                Expanded(
+                    child: ReusableCard(
+                  colour: activeCardColor,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text(
+                        'MALE',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      )
+                    ],
+                  ),
+                )),
                 Expanded(child: ReusableCard(colour: activeCardColor)),
               ],
             )),
@@ -45,13 +69,15 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.cardChild});
 
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
           color: colour, borderRadius: BorderRadius.circular(10.0)),
